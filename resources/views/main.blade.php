4f0d77ebf7b30d9082a7236ce7434aefb8bb0858
cruzvier <langgengyogi@gmail.com>
2020-09-07 18:23:21 +0700

    add card image

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -3,32 +3,104 @@
 @section('title', 'home')
 
 @section('content')
-<div class="container-fluid">
-	<div class="row">
-		<div class="col-8">
-			<div class="row">
-				<div class="col-md-4">
-						<img src="https://getbootstrap.com/docs/4.5/assets/brand/bootstrap-solid.svg">
+<div class="container-fluid" style="margin-top: 20px">
+	<div class="card-deck">
+
+		<div class="card mb-3" style="min-width: 360px;">
+			<div class="row no-gutters">
+			  <div class="col-md-4">
+				<img src="img/Katarina.jpg" class="card-img" alt="...">
+			  </div>
+			  <div class="col-md-8">
+				<div class="card-body">
+				  <h5 class="card-title">Card title</h5>
+				  <a data-toggle="collapse" href="#col-1"> collabse type </a>
+				  <p class="collapse" id="col-1">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+				  <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
 				</div>
-					<div class="col-md-4">
-						<img src="https://getbootstrap.com/docs/4.5/assets/brand/bootstrap-solid.svg">
-				</div>
-				<div class="col-md-4">
-						<img src="https://getbootstrap.com/docs/4.5/assets/brand/bootstrap-solid.svg">
-				</div>
-				<div class="col-md-4">
-					<div class="card" style="width: 18rem;">
-						<img src="https://getbootstrap.com/docs/4.5/assets/brand/bootstrap-solid.svg" class="card-img-top" alt="...">
-						<div class="card-body">
-						  <h5 class="card-title">Card title</h5>
-						  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-						  <a href="#" class="btn btn-primary">Go somewhere</a>
-						</div>
-					  </div>
-	</div>
+			  </div>
 			</div>
-	</div>
-		<div class="col-4">col-4</div>
+		  </div>
+
+		  <div class="card mb-3" style="min-width: 360px;">
+			<div class="row no-gutters">
+			  <div class="col-md-4">
+				<img src="img/Katarina.jpg" class="card-img" alt="...">
+			  </div>
+			  <div class="col-md-8">
+				<div class="card-body">
+				  <h5 class="card-title">Card title</h5>
+				  <a data-toggle="collapse" href="#col-2"> collabse type </a>
+				  <p class="collapse" id="col-2">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+				  <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+				</div>
+			  </div>
+			</div>
+		  </div>
+		
+		  <div class="card mb-3"style="min-width: 360px;">
+			<div class="row no-gutters">
+			  <div class="col-md-4">
+				<img src="img/Katarina.jpg" class="card-img" alt="...">
+			  </div>
+			  <div class="col-md-8">
+				<div class="card-body">
+				  <h5 class="card-title">Card title</h5>
+				  <a data-toggle="collapse" href="#col-3"> collabse type </a>
+				  <p class="collapse" id="col-3">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+				  <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+				</div>
+			  </div>
+			</div>
+		  </div>
+
+		  <div class="card mb-3" style="min-width: 360px;">
+			<div class="row no-gutters">
+			  <div class="col-md-4">
+				<img src="img/Katarina.jpg" class="card-img" alt="...">
+			  </div>
+			  <div class="col-md-8">
+				<div class="card-body">
+				  <h5 class="card-title">Card title</h5>
+				  <a data-toggle="collapse" href="#col-4"> collabse type </a>
+				  <p class="collapse" id="col-4">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+				  <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+				</div>
+			  </div>
+			</div>
+		  </div>
+
+		  <div class="card mb-3" style="min-width: 360px;">
+			<div class="row no-gutters">
+			  <div class="col-md-4">
+				<img src="img/Katarina.jpg" class="card-img" alt="...">
+			  </div>
+			  <div class="col-md-8">
+				<div class="card-body">
+				  <h5 class="card-title">Card title</h5>
+				  <a data-toggle="collapse" href="#col-5"> collabse type </a>
+				  <p class="collapse" id="col-5">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+				  <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+				</div>
+			  </div>
+			</div>
+		  </div>
+
+		  <div class="card mb-3" style="min-width: 360px;">
+			<div class="row no-gutters">
+			  <div class="col-md-4">
+				<img src="img/Katarina.jpg" class="card-img" alt="...">
+			  </div>
+			  <div class="col-md-8">
+				<div class="card-body">
+				  <h5 class="card-title">Card title</h5>
+				  <a data-toggle="collapse" href="#col-6"> collabse type </a>
+				  <p class="collapse" id="col-6">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+				  <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+				</div>
+			  </div>
+			</div>
+		  </div>
 	  </div>
   </div>
 @endsection
